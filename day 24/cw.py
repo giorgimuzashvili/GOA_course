9837029cb461f937b394f852416b105b20ab3ace
giorgimuzashvili <giorgimuzashvili8@gmail.com>
2024-05-10 20:43:48 +0400

    second commit

--- day 24/cw.py
+++ day 24/cw.py
@@ -1,15 +1,51 @@
-def fake_bin(x):
-    result = ''
-    for digit in x:
-        if int(digit) < 5:
-            result += '0'
-        else:
-            result += '1'
-    return result
+#valeba
+# def litres(time):
+    
+#     return int(time * 0.5)
 
-input_string = "123456789"
-result = fake_bin(input_string)
-print("Resulting string:", result)
+
+
+
+
+
+
+# 2 davaleba
+# def paperwork(n, m):
+   
+#     if n < 0 or m < 0:
+#         return 0
+#     else:
+#         return n * m
+
+# 3 davaleba
+# def grow(numbers_list):
+#     result = 1
+    
+#     for number in numbers_list:
+#         result = result * number
+    
+#     return result
+
+
+
+
+
+
+
+
+# 4 davaleba
+# def fake_bin(x):
+#     result = ""
+    
+#     for char in x:
+#         if int(char) < 5:
+#             result = result + "0"
+#         else:
+#             result = result + "1"
+    
+#     return result
+
+
 
 
 
@@ -23,8 +59,56 @@ print("Resulting string:", result)
 # def count_by(x, n):
 #      return [x * i for i in range(1, n + 1)]
 
+
+
+
+
+
+# 7kyu 1 davaleba
+# def to_jaden_case(string):
+#      return ' '.join(word.capitalize() for word in string.split())
+
+
+# quote = "how can mirrors be real if our eyes aren't real"
+# jaden_case_quote = to_jaden_case(quote)
+# print(jaden_case_quote)
+  
+
+# 7kyu 2 davaleba
+# def accum(st):
+#     return '-'.join((c.upper() + c.lower() * i) for i, c in enumerate(st))
+# result = accum("abcd")
+# print(result)  
+
+
+
+
+
+
+
+
+# 7kyu 3 davaleba 
+# def remove_smallest(numbers):
+#     if not numbers:
+#         return []
+#     min_index = numbers.index(min(numbers))
+    
+#     return numbers[:min_index] + numbers[min_index + 1:]
+# numbers = [4, 3, 1, 2, 5]
+# result = remove_smallest(numbers)
+# print("Ratings after removing the smallest exhibit:", result)
+
+
+# 6kyu 4 davaleba
+
+# def solution(number):
+#     if number < 0:
+#         return 0
+#     else:
+#         return sum(num for num in range(number) if num % 3 == 0 or num % 5 == 0)
+
 # # Example usage:
-# x_value = 5
-# count = 7
-# result = count_by(x_value, count)
-# print("First", count, "multiples of", x_value, ":", result)
+# number = 10
+# result = solution(number)
+# print("Sum of multiples of 3 or 5 below", number, ":", result)
+ 
---
+++ day 26/cw.py
@@ -0,0 +1,78 @@
+# def create_list(name, amount = 5):
+#     names = []
+
+#     for i in range(amount):
+#         names.append(name)
+    
+#     return names
+
+
+# result = create_list("nia", 3)
+# print(result)
+
+
+
+# def twice_as_old(dad_years_old, son_years_old):
+#     if dad_years_old >= 2 * son_years_old:
+#         return dad_years_old - 2 * son_years_old
+#     else:
+#         return 2 * son_years_old - dad_years_old
+    
+
+
+
+#     def reverse_seq(n):
+#     result = [] 
+#     for i in range(n, 0, -1):
+#         result.append(i)
+#     return result
+
+
+
+
+
+# def no_space(x):
+#     return x.replace(" ", "") 
+
+
+
+
+
+
+# def abbrev_name(name):
+#     first_name, last_name = name.split()
+    
+#     initials = first_name[0].upper() + '.' + last_name[0].upper()
+#     return initials
+
+
+
+
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+    
+#     else:
+#         return n * factorial(n - 1)
+    
+
+
+
+
+
+
+
+
+
+
+
+# def capitals(word):
+#     return [i for i, char in enumerate(word) if char.isupper()]
+
+
+
+
+
+
+
