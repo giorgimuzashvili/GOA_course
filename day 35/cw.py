03b9b40a5ea94435d3eaeef7d59bbaf890bc9921
giorgimuzashvili <giorgimuzashvili8@gmail.com>
2024-06-09 22:39:50 +0400

    second commit

---
+++ day 35/cw.py
@@ -0,0 +1,146 @@
+# 1)მოხმარებელს შემოატანინეთ სახელი და გვარი
+# და დარწმუნდით იმაში რომ სახელიც და გვარიც იწყება დიდი ასოთი
+# txt = "giorgi muzashvili"
+
+
+# x = txt.capitalize()
+
+# print (x)
+
+
+
+# 2)მომხმარებელსშემოატანინეთ რაიმე წინადადება და დაითვალეთ
+# რამდენჯერ მეორდება სიმბოლო 'a'.
+# txt = " ronaldo is goat ronaldo ronaldo ronaldo "
+# x = txt.count ("o")
+# print (x)
+
+
+# 3)მომხმარებელს შემოატანინეთ სახელი და მოძებნეთ სიმბოლო 'a' რომელ ინდექსზე დგას
+# გამოიყენეთ find and index მეთოდი და კომენტარების გამოყენებით ახსენით მათ შორის რა განსხვავებაა
+
+
+# name ="sgsggsgsgsgsgsgsgsgs  muzaaaaaaa"
+# x = name.index("a")
+
+# print(x)
+
+
+
+
+# 4)შეამოწმეთ რაიმე სტრინგი თუ შეიცვას მხოლოდ ასოებს და ციფრებს
+
+
+# 8)მომხმარებელს 
+# შემოატანინეთ სახელი და სახელში აირჩიეთ 
+# რომელიმე სიმბოლო და შეცვალეთ ყველა ეგ სიმბოლო 'k'_ით.
+# txt = "giorgimuzashvili"
+
+# x = txt.replace("i", "k")
+
+# print(x)
+
+
+
+
+# 7)შეამოწმეთ შედგება თუ არა სტრინგი 
+# მხოლოდ პატარა სიმბოლოებისგან შემდეგ კი 
+# შეამოწმეთ შედგება თუ არა სტრინგი მხოლოდ დიდი ასოებისგან
+
+# string = "jdjdjdjdjdjj"
+# print(string.islower())  # True
+
+# string = "ZgZgZgKZkXKZ"
+# print(string.islower())
+
+# string = "SJDFJAOSMFAUNF"
+# print(string.isupper())  # True
+
+# string = "DJHFUSISDSsd"
+# print(string.isupper()) 
+
+# 10)შემ,ოატანინეთ 
+# მოხმარებელს სახელი და გვარი ერთიანად შემდეგ კი 
+# გაყავით ისინი space ის გამოყენებით (split method)
+
+# txt = "giorgimuzashvili"
+# x = txt.split()
+
+# print(x)
+
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.append("orange")
+
+
+# 1)შექმენით სია და დაამატეთ თქვენი მეგობრების სახელები append ის გამოყენებით.
+# names = [ 'atuka','daviti', 'cotne']
+# names.append ("tornike")
+# print(names)
+# 2)შექმენით სია და ყველა ელემენტი წაშალეთ (გაასუფთავეთ)
+# siaaa= ["alpha", "sigma", "volki "]
+
+# siaaa.clear()
+
+# print(siaaa)
+# 3)შექმენით სია და დააკოპიერთ ეგ სია შემდეგ კი შეინახეთ მეორე ცვლადში
+# cars= ["bmw", "mercedes", "audi"]
+
+# x = cars.copy()
+
+# print(x)
+# 4) შექმენით სახელების სია და დათალეთ რამდენჯერ მეორდება თვქენი სასურველი სახელი სიაში
+# name = ["sudaaa", "muza", "muza"]
+
+# x = name.count("muza")
+
+# print(x)
+# 5) შექმენით სია და იპოვეთ კონკრეტული თქვენი სასურველი ელემენტის ინდექსი
+# movies = ['spiderman', 'batman', 'lspd']
+
+# x = movies.index("lspd")
+
+# print(x)
+# 6) შექმენით სია და ჩასვით 3 ინდექსზე ახალი მნიშვნელობა
+# cars = ['bmw', 'audi', 'honda']
+# cars.insert(3, "mercedes")
+
+# print(cars)
+
+# 7) წაშალეთ სიის ბოლოში და სიის დასწყიშვი ელემენტები
+# names = ["Luka","Nia", "Lile","Luka"]
+# names.pop()
+# names.pop(0)
+
+# print(names)
+# 8) წაშალეთ ელემენტი remove მეთოდის გამოყენებით
+# names = ['atuka', 'saba', 'gio']
+
+# names.remove("atuka" )
+
+# print(names)
+
+# 9) შემოაბრუნეთ სია
+# names = ['atuka', 'saba', 'gio']
+
+# names.reverse( )
+
+# print(names)
+
+# 10) დაალაგეთ რიცხვების სია ზრდადობით
+# numbers = [5, 4, 10, 12, 3]
+
+# numbers.sort()
+
+# print(numbers)
+
+# 11) გააფართოვეთ პირველი სია მეორე სიით
+# movies = ['spiderman ', 'batman', 'doctor house']
+
+# names= ['gio', 'saba', 'luka']
+
+# movies.extend(names)
+
+# print(movies)
+
+
---
+++ day 35/hw.py
@@ -0,0 +1,242 @@
+#  1)Initialize an empty list and append elements from multiple smaller lists to it.
+# cars = ["bmw" , "mercedes" ,"audi"]
+# cars.append("honda")
+# print(cars)
+
+
+
+
+
+#  2)Initialize a list and then use the clear() method to remove all elements, making it empty.
+
+# movies = ["the walking dead" "ricer" "bikers"]
+
+# movies.clear()
+# print (movies)
+
+
+
+# 3)Create a list of items, then use the copy() method to make a duplicate list, serving as a backup.
+
+# items = ["clock" , "perfume" , "dummbells"]
+
+# x=items.copy()
+# print (x)
+
+# 4)Create a program that counts the number of occurrences \
+#     of a specific character in a given string using the count() method.|
+
+# names = ["atuka" , "giorgi" , "luka"]
+# x= names.count ("atuka")
+# print (x)
+
+
+
+# 5)Develop a program that finds the index of the first
+# occurrence of a specific element in a list using the index() method
+
+# names = ["atuka" , "giorgi" , "luka"]
+
+# x=names.index ("luka")
+# print(x)
+
+
+# 6)Create a program that inserts a new element into a list at a specified index using the insert() method.
+
+# cars= ["bmw" , "mercedes" , "honda"]
+# cars.insert (2,  "audi")
+# print (cars)
+
+# 7)Create a program that removes and returns the last element from a list using the pop() method.
+
+# movies = ["spiderman" , "batman" , "starwars"]
+# movies.pop  (1)
+# print (movies)
+
+# 8)Write a program that removes a specific element from a list using the remove() method.
+
+# cars = ["bmw" , "mercedes" , "honda"]
+
+# cars.remove ("mercedes")
+# print (cars)
+
+
+
+# 9)Create a list of numbers and then use the reverse() method
+# to reverse the order of elements in the list.
+
+# names = ["mari" , "nia" , "kato"]
+
+# names.reverse ()
+# print(names)
+
+# 10)Create a list of numbers and then use the sort() method to sort them in ascending order.
+# numbers= [1 ,2 ,5 ,4 ,7 ,6 ,9 ,8]
+# numbers.sort()
+# print(numbers)
+
+# 11)Create a list and use the len() function to count the number of elements in it.
+# list = [ "apple" , "banana" ,"cherry"]
+# x=len(list)
+# print (x)
+
+
+# 12)Create two lists and use the extend() method to append elements of one list to another.
+
+# cars = ["bmw" , "corvette" ,"mercedes"]
+# movies = ["spiderman" , "batman" , "grinch"]
+# cars.extend(movies)
+# print(cars)
+
+
+# 13)Take a word as input and use the capitalize() method to capitalize the first letter.
+# txt = "helloo buddyy  its  georgiaaa"
+
+# x = txt.capitalize()
+
+# print (x)
+
+
+
+
+# 14)Take a string as input and use the count() method to count the occurrences of a specific character.
+
+# txt = " georgia georgia georgia georgia georgia"
+
+# x = txt.count("georgia")
+
+# print(x)
+
+
+# 15)Take a string as input and use the index() method
+# to find the index of the first occurrence of a specific character.
+
+# fruits = ['apple', 'banana', 'cherry']
+
+# x = fruits.index("apple")
+
+# print(x)
+
+
+# 16)Input the user to enter a username and use the isalnum() 
+# method to verify if it contains only alphanumeric characters.
+
+
+# txt = "muzaaaa"
+
+# x = txt.isalnum()
+
+# print(x)
+
+
+# 17)Given a list of strings, filter out the strings that contain 
+# non-alphabetic characters using the isalpha() method
+
+# txt = "sgsgsgsgsggsgs"
+
+# x = txt.isalpha()
+
+# print(x)
+
+# 18)Input the user to enter a numeric value and use the
+# isdigit() method to verify if it consists only of digits
+
+
+
+# txt = "380005"
+
+# x = txt.isdigit()
+
+# print(x)
+
+
+
+# 19)input the user to enter a string and use the islower()
+# method to verify if it consists only of lowercase letters.
+
+
+# txt = "dila mshvidobisa saqartvelooo"
+
+# x = txt.islower()
+
+# print(x)
+
+
+
+
+# 20)Verify if a word consists only of uppercase letters using
+# the isupper() method before further processing or analysis.
+
+
+# txt = "erbo erboa qoni qonia xo"
+
+# x = txt.isupper()
+
+# print(x)
+
+
+
+# 21)Convert a user's input to lowercase using the lower()
+# method to ensure uniformity for further processing.
+
+
+# txt = "rais dzmakacebi tqven xart simonn"
+
+# x = txt.lower()
+
+# print(x)
+
+# 22)Given a string, replace all occurrences of a specified
+# substring with another substring using the replace() method.
+
+# txt = "I like corvette"
+
+# x = txt.replace("corvette", "bmwwww")
+
+# print(x)
+
+
+
+
+# 23)Given a sentence, split it into individual words using the split() method for further analysis.
+
+# txt = "mobrdzandi simon chems saxlshii"
+
+# x = txt.split()
+
+# print(x)
+
+
+
+
+# 24)Given a list of strings, filter out the strings that start
+# with a specific prefix using the startswith() method
+
+
+# txt = "bmw chemi cxovrebaa chemi dedas vficavar"
+# x = txt.startswith("bmw")
+
+# print(x)
+
+
+# 25)Given a string, check if it ends with a specific character or substring using the endswith() method.
+
+# txt = "gamarjobat sazogadoebav."
+
+# x = txt.endswith(".")
+
+# print(x)
+
+
+# 26)Given a string, convert all uppercase letters to lowercase and vice versa using the swapcase() method.
+
+
+# txt = "gaumarjos chems dzmasss"
+
+# x = txt.swapcase()
+
+# print(x)
+
+
+
+
